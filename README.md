c35ba63c5df3e6c06d5d634627fc00039a0ec6b2
ZachW <zach@hertzian.co.uk>
2019-12-16 11:47:26 +0000

    Added modal functionality and display, example updated

--- README.md
+++ README.md
@@ -1,3 +1,36 @@
 # jq-modal  
 
-Simple modal box plugin for jQuery.
+Simple modal box plugin for jQuery.  
+
+## Usage  
+
+To open a modal, use:  
+
+```javascript
+$('.selector').jmodal({});
+```
+
+This will open an empty modal which will be able to close, and will use a default sizing.  
+
+The Object passed to the function can contain any of the following options.  
+
+- `height`: A String measurement or function returning such to determine the height of the modal. _default: auto;_  
+- `width`: A String measurement or function returning such to determine the width of the modal. _default: 300px;_  
+- `header`: A String, DOM element, jQuery object, or function returning any of the previous types to create a header for the modal. _default: undefined;_  
+- `body`: A String, DOM element, jQuery object, or function returning any of the previous types to create the content of the modal. _default: undefined;_  
+- `footer`: A String, DOM element, jQuery object, or function returning any of the previous types to create a footer for the modal. _default: undefined;_  
+- `class`: A String class name to append to the modal box for styling purposes. _default: '';_  
+- `closetext`: String text to insert into close button. _default: "Close";_  
+
+## Events  
+
+The modal fires some events with its usage, including when opened and closed.  
+
+- `modal-triggered`: Before the modal is opened/closed.  
+- `modal-opened`: When the modal is opened, after rendered and ready.  
+- `modal-closed`: When the modal has been closed.  
+
+## Override defaults  
+
+Some default classes, ids, and event names can be modified by changing values in the exposed defaults object: `jmodalDefaults`.  
+In most cases, these values should not need to be changed.  
--- src/js/jq-modal.js
+++ src/js/jq-modal.js
@@ -1,13 +1,131 @@
 /**
- * A JS solution to handle many modals, and
- * creating new ones.
+ * A JS solution to handle modals.
  */
+var jmodalDefaults = {
+    modalopencls: 'jqmodal-open',
+    overlayid: 'jqmodal-overlay',
+    parentid: 'jqmodal',
+    actionbarcls: 'jqmodal-action',
+    closecls: 'jqmodal-close',
+    headercls: 'jqmodal-header',
+    bodycls: 'jqmodal-body',
+    footercls: 'jqmodal-footer',
+    triggerevt: 'modal-triggered',
+    openevt: 'modal-opened',
+    closeevt: 'modal-closed',
+    closetext: 'Close',
+    height: "auto",
+    width: "300px",
+};
+
 (function($) {
 
-    var fn = {};
+    var fn = {
+        create: function() {
+            // overlay (base of modal)
+            var $modal = $("<div></div>");
+            $modal.attr({id: jmodalDefaults.overlayid}).hide();
 
-    $.fn.jmodal = function() {
-        
+            // add to body
+            $("body").append($modal);
+            return $modal;
+        },
+        open: function($modal, options) {
+            // triggered
+            $('body').trigger(jmodalDefaults.triggerevt);
+            // height/width
+            var height, width;
+            if (!options.height) height = jmodalDefaults.height;
+            else if (typeof options.height === "function") height = options.height();
+            else height = options.height;
+            if (!options.width) width = jmodalDefaults.width;
+            else if (typeof options.width === "function") width = options.width();
+            else width = options.width;
+            // body
+            $body = $("<div></div>");
+            var attrs = {id: jmodalDefaults.parentid};
+            var style = {height: height, width: width};
+            $body.attr(attrs).css(style);
+            if (options.class) $body.addClass(options.class);
+            $modal.append($body);
+            // header/content/footer
+            fn.header($body, options.header, options.closetext);
+            fn.body($body, options.body);
+            fn.footer($body, options.footer);
+            // show and assign close
+            $modal.show();
+            $modal.off('click').on("click", function(evt) {
+                // close if not child of overlay
+                if ($(evt.target).attr('id') === jmodalDefaults.overlayid) {
+                    fn.close($modal);  // close
+                }
+            });
+            // no scroll class
+            $('body').addClass(jmodalDefaults.modalopencls);
+            // event
+            $('body').trigger(jmodalDefaults.openevt);
+        },
+        close: function($modal) {
+            // triggered
+            $('body').trigger(jmodalDefaults.triggerevt);
+            // clear content & class
+            $modal.empty().hide();
+            $('body').removeClass(jmodalDefaults.modalopencls);
+            // event
+            $('body').trigger(jmodalDefaults.closeevt);
+        },
+        header: function($body, head, txt) {
+            // Add controls
+            var $headBar = $('<div></div>');
+            $headBar.addClass(jmodalDefaults.actionbarcls);
+            // close button
+            if (!txt) txt = jmodalDefaults.closetext;
+            var $close = $("<span></span>");
+            $close.addClass(jmodalDefaults.closecls).text(txt);
+            $close.one("click", function() { fn.close($body.parent()); });
+            $headBar.append($close);
+            $body.append($headBar);
+            // Do nothing if no head
+            if (typeof head === "undefined") return;
+            // define
+            var $header = $("<div></div>");
+            $header.addClass(jmodalDefaults.headercls);
+            // add content
+            $header.append(head);
+            // add
+            $body.append($header);
+        },
+        body: function($body, body) {
+            // define
+            var $content = $("<div></div>");
+            $content.addClass(jmodalDefaults.bodycls);
+            $body.append($content);
+            // Body element is always added.
+            if (typeof body === "undefined") return;
+            // Add content if given
+            $content.append(body);
+        },
+        footer: function($body, footer) {
+            // do nothing if no footer
+            if (typeof footer === "undefined") return;
+            // define
+            var $footer = $("<div></div>");
+            $footer.addClass(jmodalDefaults.footercls);
+            $body.append($footer);
+            // Add content
+            $footer.append(footer);
+        },
+    };
+
+    $.fn.jmodal = function(options) {
+        if (typeof options === "object") {
+            // find or create
+            var $modal = $('body').find('#'+jmodalDefaults.overlayid);
+            if ($modal.length === 0) $modal = fn.create(options);
+            // create internal
+            fn.open($modal, options);
+        }
+        return this;
     };
 
 })(jQuery);
